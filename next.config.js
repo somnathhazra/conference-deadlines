21521cb0d1d4c639e5794d9111f07730907d51fb
Somnath Hazra <49022390+somnathhazra@users.noreply.github.com>
2024-11-10 15:37:04 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,10 @@
+```javascript
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  basePath: '/conference-deadlines',
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+}

feea0c38044caad38cf6d6b2ef1d09ca6f45eba9
Somnath Hazra <49022390+somnathhazra@users.noreply.github.com>
2024-11-10 16:14:35 +0530

    Create conferences.js

---
+++ conferences.js
@@ -0,0 +1,28 @@
+// Conference data
+const conferences = [
+    {
+        name: "ICML 2025",
+        deadline: "2025-01-31",
+        url: "https://icml.cc/"
+    },
+    {
+        name: "NeurIPS 2024",
+        deadline: "2024-01-25",
+        url: "https://neurips.cc/"
+    },
+    {
+        name: "ICLR 2024",
+        deadline: "2024-02-15",
+        url: "https://iclr.cc/"
+    },
+    {
+        name: "AAAI 2024",
+        deadline: "2024-03-20",
+        url: "https://aaai.org/conference/"
+    },
+    {
+        name: "AAMAS 2024",
+        deadline: "2024-04-10",
+        url: "https://aamas2024-conference.auckland.ac.nz/"
+    }
+];

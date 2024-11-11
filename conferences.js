49ee29d00e816b83b1e1ebad9c64ac3d1e8c7b64
Somnath Hazra <49022390+somnathhazra@users.noreply.github.com>
2024-11-11 12:25:14 +0530

    Update conferences.js

--- conferences.js
+++ conferences.js
@@ -27,7 +27,7 @@ async function fetchConferenceDataCSV() {
         const data = await response.text();
 
         deadlines = parseCSV(data); // Populate the global deadlines array
-        console.log(deadlines);
+        console.log(parseCSV(data));
     } catch (error) {
         console.error("Error fetching data from CSV:", error);
     }

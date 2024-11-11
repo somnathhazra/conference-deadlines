526c7c2bd2e0404ff64111ac009c422946e20594
Somnath Hazra <49022390+somnathhazra@users.noreply.github.com>
2024-11-11 12:00:02 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -19,7 +19,7 @@ function updateCalendarWithDeadlines() {
 // Call this after fetchConferenceDataCSV has populated deadlines
 setTimeout(updateCalendarWithDeadlines, 1000);
 
-console.log(deadlines)
+console.log(deadlines);
 
 const today = new Date();
 

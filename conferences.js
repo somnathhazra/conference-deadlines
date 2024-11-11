c9fad27614ca6f19b3611567e0ff82af0e930b6f
Somnath Hazra <49022390+somnathhazra@users.noreply.github.com>
2024-11-11 13:38:00 +0530

    Update conferences.js

--- conferences.js
+++ conferences.js
@@ -4,12 +4,12 @@
 const deadlines = [
     {
         conference: "ICML 2025 Abstract",
-        date: "2025/01/31",
+        date: "2025-01-31",
         link: "https://icml.cc/"
     },
     {
         conference: "ICLR 2025",
-        date: "2024/10/01",
+        date: "2024-10-01",
         link: "https://iclr.cc/"
     },
 ];
